689fce9d001557f988ab7d2aae4838741e68f5fa
Tyler Jewell <jewelltyler15@gmail.com>
2021-03-25 11:08:43 -0400

    final

--- friendsjs/friends.js
+++ friendsjs/friends.js
@@ -1,13 +1,26 @@
-let friends=[];
+let friends = [];
 
-const display = () =>{
-    let divCtrl=document.getElementById("output");
-    for( let friend of friends){
-        let L=`<p>${friend}</p>`;
-        divCtrl.innerHTML += L;
+const submit =() =>{
+    let fname=document.getElementById("name").value;
+    let femail=document.getElementById("email").value;
+    let fphone=document.getElementById("phone").value;
+    let friend ={
+        name: fname, email: femail, phone: fphone
     }
-};
-const submit = () =>{
-friends.push(document.getElementById("form").value)
-display();
+    friends.push(friend);
+    display();
+}
+
+const display=()=>{
+    let tbody = document.getElementById("tbody");
+    tbody.innerHTML ="";
+    for(let friend of friends){
+        let tr = "<tr>";
+        tr += `<td>${friend.name}</td>`;
+        tr += `<td>${friend.email}</td>`;
+        tr += `<td>${friend.phone}</td>`;
+        tr +="</tr>"
+        tbody.innerHTML += tr;
+    }
+
 }
